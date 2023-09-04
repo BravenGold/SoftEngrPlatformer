3eb008b332aaa5ef697a71d82d3bab3df4c60f02
SpencerHReed <124669632+SpencerHReed@users.noreply.github.com>
2023-09-03 19:45:06 -0700

    Added Ui, Added Pickups
    Added Healthbar, points system, lives and collectables

---
+++ Assets/Code/Trigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Trigger : MonoBehaviour
+{
+    public EventTrigger.TriggerEvent TheTrigger;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Trigger");
+
+            BaseEventData EventData = new BaseEventData(EventSystem.current);
+            this.TheTrigger.Invoke(EventData);
+        }
+    }
+}
